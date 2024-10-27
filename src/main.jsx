33f56fab7ef05e93125987ecf045bf54002fb8b1
CamiloGs-univalle <180302644+CamiloGs-univalle@users.noreply.github.com>
2024-10-27 12:07:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./assets/pages/login/Login.jsx";
+import Quiz from "./assets/pages/quiz/Quiz.jsx";
+import Home from "./assets/pages/home/Home.jsx";
+import RecyCling from "./assets/pages/recycling/Recycling.jsx";
+import Page404 from "./assets/pages/page-404/Page404.jsx";
+import Erosion from "./assets/pages/earth/soil-erosion/erosion.jsx";
+import Management from "./assets/pages/earth/improper-waste-management/management.jsx";
+import Map from "./assets/pages/map/map.jsx";
+
+const router = createBrowserRouter([
+  { path: "/login",element: <Login />,},
+  { path: "/Quiz", element: <Quiz />,},
+  { path: "/home", element: <Home /> },
+  {path: "/soil-erosion", element: <Erosion/>},
+  { path: "/recycling", element: <RecyCling /> },
+  { path: "*", element: <Page404 /> },
+  { path: "/management", element: <Management /> },
+  { path: "/map", element: <Map /> },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
